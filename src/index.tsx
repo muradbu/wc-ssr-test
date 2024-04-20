95b163d10f688f3389882537f8b78962b9938ce0
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2024-04-20 16:15:32 +0200

    aaaaa

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ function index() {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Web Components SSR Example</title>
-        <script src="./public/wc-ssr/CustomComponent.js" defer></script>
+        <script src="./public/CustomComponent.js" defer></script>
       </head>
       <body>
         <h1>Hello World!</h1>
